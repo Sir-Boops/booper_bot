c701917ae234a2a67b95e459ebb7da34d51af8ab
Sir_Boops <admin@boops.me>
2019-03-27 15:09:19 -0600

    It kinda works

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 // Imports
 const WebSocket = require('ws');
+const fs = require('fs')
+
+// Init webdocket
+const ws = new WebSocket('wss://irc-ws.chat.twitch.tv')
+
+// When connection is opened
+ws.on('open', function open() {
+  // Load the config and read the options
+  fs.readFile('./config.json', 'utf8', (err, data) => {
+    if (err) throw err
+    ws.send('PASS ' + JSON.parse(data).pass)
+    ws.send('NICK ' + JSON.parse(data).user)
+    ws.send('JOIN ' + JSON.parse(data).channel)
+  })
+})
+
+ws.on('message', function incoming(data) {
+
+  data_clean = data.replace(/(\r\n|\r|\n)/g, "").trim()
+
+  // Reply to PINGs
+  if (data_clean === 'PING :tmi.twitch.tv') {
+    ws.send('PONG :tmi.twitch.tv')
+    console.log('Replied to a ping')
+  }
+
+  // If channel message
+  if (data_clean.indexOf('PRIVMSG') > -1) {
+    user = data_clean.match(/(.*)\!/g)[0].replace(':', '').replace('!', '')
+    message = data_clean.substring(data_clean.indexOf("PRIVMSG")).match(/:.+/g)[0].replace(':', '')
+    console.log(user + ": " + message)
+  }
+})
